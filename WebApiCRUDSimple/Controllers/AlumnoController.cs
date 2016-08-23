8e9eabd4a2f85ee9455764c23b4dd7958fbc3538
Hacybeyker <Hacybeyker@HACYBEYKER>
2016-08-22 19:17:25 -0500

    Implementacion del modelo Alumno y generacion del controlador del mismo

---
+++ WebApiCRUDSimple/Controllers/AlumnoController.cs
@@ -0,0 +1,114 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApiCRUDSimple.Models;
+
+namespace WebApiCRUDSimple.Controllers
+{
+    public class AlumnoController : ApiController
+    {
+        private readonly AlumnoDbContext _db = new AlumnoDbContext();
+
+        // GET: api/Alumno
+        public IQueryable<Alumno> GetAlumnos()
+        {
+            return _db.Alumnos;
+        }
+
+        // GET: api/Alumno/5
+        [ResponseType(typeof(Alumno))]
+        public IHttpActionResult GetAlumno(int id)
+        {
+            var alumno = _db.Alumnos.Find(id);
+            if (alumno == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(alumno);
+        }
+
+        // PUT: api/Alumno/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutAlumno(int id, Alumno alumno)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != alumno.Id)
+            {
+                return BadRequest();
+            }
+
+            _db.Entry(alumno).State = EntityState.Modified;
+
+            try
+            {
+                _db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AlumnoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Alumno
+        [ResponseType(typeof(Alumno))]
+        public IHttpActionResult PostAlumno(Alumno alumno)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _db.Alumnos.Add(alumno);
+            _db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = alumno.Id }, alumno);
+        }
+
+        // DELETE: api/Alumno/5
+        [ResponseType(typeof(Alumno))]
+        public IHttpActionResult DeleteAlumno(int id)
+        {
+            Alumno alumno = _db.Alumnos.Find(id);
+            if (alumno == null)
+            {
+                return NotFound();
+            }
+
+            _db.Alumnos.Remove(alumno);
+            _db.SaveChanges();
+
+            return Ok(alumno);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool AlumnoExists(int id)
+        {
+            return _db.Alumnos.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ WebApiCRUDSimple/Models/Alumno.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+
+namespace WebApiCRUDSimple.Models
+{
+    public class Alumno
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Dni { get; set; }
+        public string Telefono { get; set; }
+    }
+    internal class AlumnoDbContext : DbContext
+    {
+        public DbSet<Alumno> Alumnos { get; set; }
+    }
+}
